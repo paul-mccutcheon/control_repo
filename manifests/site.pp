5109c3ec6993eb50a92e8920460a8cafe59e3fb7
paul-mccutcheon <56485149+paul-mccutcheon@users.noreply.github.com>
2019-10-14 21:49:49 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,11 +6,7 @@ node 'master.puppet.vm' {
     ensure => file,
     content => "Welcome to ${fqdn}\n",
    }
-   file { '/etc/hosts':
-    ensure => file,
-    content =>  "172.18.0.3 web web.puppet.vm\n172.18.0.2 db db.puppet.vm\n",              
    }
-}
 
 node /^web/ {
   include role::app_server
